54948bf1a50a8bd598ce74eb44431b48e90f9e43
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2025-03-30 15:46:01 +0800

    feat: add delete confirmation pop up

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,23 +1,38 @@
 import React from "react";
-
+import Swal from "sweetalert2";
 const users = [
   {
     id: 1,
-    name: "Administrator",
+    name: "ADMINISTRATOR",
     email: "admin@gmail.com",
     gender: "Male",
     phone: "09051234567",
     position: "Admin",
   },
   {
-    id: 1,
+    id: 2,
     name: "JUAN DELA CRUZ",
     email: "juandelacruz@gmail.com",
     gender: "Male",
     phone: "09051234567",
-    position: "Emgineer",
+    position: "Engineer",
   },
 ];
+
+const handleDelete = (id) => {
+  Swal.fire({
+    icon: "question",
+    title: "Are you sure to delete this user?",
+    showCancelButton: true,
+    confirmButtonText: "Yes",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire("Saved!", "", "success");
+    } else if (result.isDenied) {
+      Swal.fire("Changes are not saved", "", "info");
+    }
+  });
+};
 
 const Users = () => {
   return (
@@ -25,20 +40,14 @@ const Users = () => {
       <div className="table-responsive mt-3">
         <table className="table table-bordered table-hover">
           <thead>
-            <tr className="text-dark">
-              <th className="text-center text-dark fw-normal p-1 m-0">ID</th>
-              <th className="text-center text-dark fw-normal p-1 m-0">Name</th>
-              <th className="text-center text-dark fw-normal p-1 m-0">Email</th>
-              <th className="text-center text-dark fw-normal p-1 m-0">
-                Gender
-              </th>
-              <th className="text-center text-dark fw-normal p-1 m-0">Phone</th>
-              <th className="text-center text-dark fw-normal p-1 m-0">
-                Position
-              </th>
-              <th className="text-center text-dark fw-normal p-1 m-0">
-                Option
-              </th>
+            <tr className="text-dark fw-bold bg-primary">
+              <th className="text-center text-white p-1 m-0">ID</th>
+              <th className="text-center text-white p-1 m-0">Name</th>
+              <th className="text-center text-white p-1 m-0">Email</th>
+              <th className="text-center text-white p-1 m-0">Gender</th>
+              <th className="text-center text-white p-1 m-0">Phone</th>
+              <th className="text-center text-white p-1 m-0">Position</th>
+              <th className="text-center text-white p-1 m-0">Option</th>
             </tr>
           </thead>
           <tbody>
@@ -65,7 +74,12 @@ const Users = () => {
                 <td className="text-center align-middle fw-normal p-1 m-0">
                   <div className="d-flex justify-content-center align-items-center gap-2">
                     <button className="btn btn-primary btn-sm">Update</button>
-                    <button className="btn btn-danger btn-sm">Delete</button>
+                    <button
+                      className="btn btn-danger btn-sm"
+                      onClick={() => handleDelete(u.id)}
+                    >
+                      Delete
+                    </button>
                   </div>
                 </td>
               </tr>
