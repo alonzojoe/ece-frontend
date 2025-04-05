a6e7addbd278a921e28cff90f07607c6c863a30a
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2025-04-05 23:58:13 +0800

    feat: create new user context for global authenticated user

---
+++ src/context/user-provider.jsx
@@ -0,0 +1,26 @@
+import UserContext from "./user-context";
+import { useState } from "react";
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const storeUser = (value) => {
+    setUser(value);
+  };
+
+  const clearUser = () => {
+    setUser(null);
+  };
+
+  const userValue = {
+    user,
+    storeUser,
+    clearUser,
+  };
+
+  return (
+    <UserContext.Provider value={userValue}>{children}</UserContext.Provider>
+  );
+};
+
+export default UserProvider;
--- src/middleware/ProtectedRoutes.jsx
+++ src/middleware/ProtectedRoutes.jsx
@@ -1,15 +1,21 @@
 import { Outlet } from "react-router-dom";
 import { getLocalStorage } from "@/libs/utils";
 import { setupToken } from "@/services/api";
+import { useContext } from "react";
+import UserContext from "@/context/user-context";
 
 const ProtectedRoutes = () => {
+  const { storeUser } = useContext(UserContext);
 
   const authToken = getLocalStorage("auth-token");
+  const authUser = getLocalStorage("auth-user");
   console.log("authToken", authToken);
+  console.log("authUser", authUser);
   if (!authToken) {
-    window.location.href = "http://localhost:5173"
+    window.location.href = "http://localhost:5173";
     return;
   }
+  storeUser(authUser);
   setupToken(authToken);
   return <Outlet />;
 };
