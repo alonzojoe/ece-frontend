53adc2288ac62570fae43319f41da842494c43e1
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-04-15 09:54:06 +0800

    feat: add a back button and useNavigate on onClick event

--- src/pages/Others/NotFound.jsx
+++ src/pages/Others/NotFound.jsx
@@ -1,7 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="error-page  d-flex align-items-center justify-content-center">
+    <div className="error-page d-flex flex-column align-items-center justify-content-center">
       <h1>404 Page not found</h1>
+      <button className="btn btn-primary" onClick={() => navigate("/home")}>
+        Go back
+      </button>
     </div>
   );
 };
